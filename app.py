7335e025fe9ebb460868db637d6d97bcf8f29f5f
ShaikAsiya-github <2003shaikasiya@gmail.com>
2025-07-15 21:29:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, request
-
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-from keras.applications.vgg16 import preprocess_input
-from keras.applications.vgg16 import decode_predictions
-#from keras.applications.vgg16 import VGG16
-from keras.applications.resnet50 import ResNet50
+import os
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+from tensorflow.keras.applications.resnet50 import ResNet50
 
 app = Flask(__name__)
 model = ResNet50()
@@ -16,7 +13,8 @@ def hello_word():
 
 @app.route('/', methods=['POST'])
 def predict():
-    imagefile= request.files['imagefile']
+    imagefile = request.files['imagefile']
+    os.makedirs('./images', exist_ok=True)
     image_path = "./images/" + imagefile.filename
     imagefile.save(image_path)
 
@@ -28,11 +26,9 @@ def predict():
     label = decode_predictions(yhat)
     label = label[0][0]
 
-    classification = '%s (%.2f%%)' % (label[1], label[2]*100)
-
+    classification = '%s (%.2f%%)' % (label[1], label[2] * 100)
 
     return render_template('index.html', prediction=classification)
-
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
